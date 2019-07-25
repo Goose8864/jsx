d66c50264f392ff4dd7e337b606119ce4269b5ad
Ryan <ryragusa@gmail.com>
2019-07-24 21:34:45 -0500

    this is code for displaying

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,12 +1,55 @@
+// Import the React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Create a react component
+// This is a function based component
+// component that using the function keyword
+/*
+const App = function() {
+    return <div>Hi there!</div>
+};
+*/
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+/*
+// we could call this function inside the button to display 'click on me'
+function getButtonText() {
+    return 'Click on me!';
+}
+*/
+
+// ES2015 arrow function
+// most common convention for multi line
+// the "style" format in the button is different using JSX vs HTML
+// you are not supposed to use the keywork "class" inside a JSX element
+// instead we use "className"
+// We do this to not confuse the class declaration
+// we can reference different JavaScript variables inside our JSX
+// we can also call a function inside of our JSX
+// when using JSX we also change the "for" parameter to "htmlFor"
+const App = () => {
+    const buttonText = { text: 'Click me' };
+    const labelText = 'Enter name:';
+
+    // this is creating a style object and referencing it into the button style 
+    // the JSX standard is creating an object for the styling 
+    // const style = { backgroundColor: "blue", color: "white" };
+
+    return (
+        <div>
+            <label className="label" htmlFor="name">
+                {labelText}
+            </label>
+            <input id="name" type="text" />
+            <button style={{ backgroundColor: "blue", color: "white" }}>
+                {buttonText.text}
+            </button>
+        </div>
+    );
+};
+
+// Take the react component and show it on the screen 
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+);
